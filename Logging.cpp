5004bd7f8a99c2f9f0461e744840b30df0036f0f
Craig Phillips <craig.phillips88@ntlworld.com>
2016-01-10 15:22:26 +0000

    Report length of key

--- Logging.cpp
+++ Logging.cpp
@@ -10,7 +10,7 @@ int send_to_emon(const char * pname, int value)
 	CURL *curl;
 	CURLcode res;
 	
-	fprintf(stderr, "Emon Key: %s\n", EmonKey);
+	fprintf(stderr, "Emon Key: %s (%u)\n", EmonKey, strlen(EmonKey));
 	
 	if (strlen(EmonKey) != 32)
 		return -1;
